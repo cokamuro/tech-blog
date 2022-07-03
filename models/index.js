cc122b011e87f3ffaeecbcadb248577102aacbfa
Chris Okamuro <cokamuro@gmail.com>
2022-07-03 15:20:21 -0400

    set model relationships for user, post, and comment

--- models/index.js
+++ models/index.js
@@ -2,13 +2,26 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-// User.hasMany(Project, {
-//   foreignKey: 'user_id',
-//   onDelete: 'CASCADE'
-// });
+User.hasMany(Post, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
 
-// Project.belongsTo(User, {
-//   foreignKey: 'user_id'
-// });
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
 
 module.exports = { User, Post, Comment };
