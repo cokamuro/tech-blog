1e09752eadea79aae21548beba7e42fdaf65475f
Chris Okamuro <cokamuro@gmail.com>
2022-07-03 15:17:48 -0400

    created post and comment models

--- models/index.js
+++ models/index.js
@@ -1,13 +1,14 @@
 const User = require('./User');
-const Project = require('./Project');
+const Post = require('./Post');
+const Comment = require('./Comment');
 
-User.hasMany(Project, {
-  foreignKey: 'user_id',
-  onDelete: 'CASCADE'
-});
+// User.hasMany(Project, {
+//   foreignKey: 'user_id',
+//   onDelete: 'CASCADE'
+// });
 
-Project.belongsTo(User, {
-  foreignKey: 'user_id'
-});
+// Project.belongsTo(User, {
+//   foreignKey: 'user_id'
+// });
 
-module.exports = { User, Project };
+module.exports = { User, Post, Comment };
